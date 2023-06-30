9dc375e82395b2e0344e998fea34872ca64cdad2
Prokopenko Nikita <117819655+prokopenko-nikita-dev@users.noreply.github.com>
2023-06-30 18:10:19 +0300

    1 version

---
+++ app.js
@@ -0,0 +1,17 @@
+// app.js — входной файл
+
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+// подключаемся к серверу mongo
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+    useFindAndModify: false
+});
+
+// подключаем мидлвары, роуты и всё остальное...
+
+app.listen(3000);
