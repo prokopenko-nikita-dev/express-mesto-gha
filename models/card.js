6292079502be002f8e0b66ef26604927f29e741f
Prokopenko Nikita <117819655+prokopenko-nikita-dev@users.noreply.github.com>
2023-07-07 12:46:15 +0300

    1.7 version

--- models/card.js
+++ models/card.js
@@ -4,8 +4,8 @@ const cardSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
-    minlength: 2,
-    maxlength: 30,
+    minlength: [2, 'Must be at least 2, you got {VALUE}'],
+    maxlength: [30, 'Must be no more than 30, you got {VALUE}'],
   },
   link: {
     type: String,
--- models/user.js
+++ models/user.js
@@ -3,13 +3,13 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    minlength: 2,
-    maxlength: 30,
+    minlength: [2, 'Must be at least 2, you got {VALUE}'],
+    maxlength: [30, 'Must be no more than 30, you got {VALUE}'],
   },
   about: {
     type: String,
-    minlength: 2,
-    maxlength: 30,
+    minlength: [2, 'Must be at least 2, you got {VALUE}'],
+    maxlength: [30, 'Must be no more than 30, you got {VALUE}'],
   },
   avatar: {
     type: String,
