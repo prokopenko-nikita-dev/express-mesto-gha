6a738b03dd6134ec961d7b9ab55f2cd3e706f068
Prokopenko Nikita <117819655+prokopenko-nikita-dev@users.noreply.github.com>
2023-07-11 14:23:26 +0300

    2.3 version

--- app.js
+++ app.js
@@ -5,12 +5,14 @@ const rateLimit = require('express-rate-limit');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const { errors } = require('celebrate');
-const users = require('./routes/users');
-const cards = require('./routes/cards');
 const signup = require('./routes/signup');
 const signin = require('./routes/signin');
 const auth = require('./middlewares/auth');
+const users = require('./routes/users');
+const cards = require('./routes/cards');
 const NotFoundError = require('./errors/notFoundError');
+
+const { PORT = 3000, BASE_PATH } = process.env;
 
 const app = express();
 
@@ -23,6 +25,15 @@ app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
+
+app.use('/signup', signup);
+app.use('/signin', signin);
+
+app.use(auth);
+
+app.use('/users', users);
+app.use('/cards', cards);
+
 app.use('/*', () => {
   throw new NotFoundError('Страница не найдена');
 });
@@ -36,24 +47,16 @@ app.use((err, req, res, next) => {
   next();
 });
 
-mongoose.connect('mongodb://localhost:27017/mestodb');
+async function main() {
+  await mongoose.connect('mongodb://localhost:27017/mestodb', {
+    useNewUrlParser: true,
+  });
+  console.log('Connected to db');
+  await app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}`);
+    console.log('Ссылка на сервер');
+    console.log(BASE_PATH);
+  });
+}
 
-app.use('/signup', signup);
-app.use('/signin', signin);
-
-app.use(auth);
-
-app.use(bodyParser.json());
-app.use('/users', users);
-app.use('/cards', cards);
-
-app.use((req, res, next) => {
-  req.user = {
-    _id: '64a3065b2a3e6673e24b4cb3',
-  };
-  next();
-});
-
-app.listen(3000, () => {
-  console.log('Сервер запущен!');
-});
+main();
