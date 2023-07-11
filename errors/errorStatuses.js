6835cfd41621db15f4aa776fdcedff251987485b
Prokopenko Nikita <117819655+prokopenko-nikita-dev@users.noreply.github.com>
2023-07-11 12:25:31 +0300

    2.2 version

--- errors/errorStatuses.js
+++ errors/errorStatuses.js
@@ -1,9 +1,17 @@
+const CREATED = 201;
 const BAD_REQUEST = 400;
+const UNAUTHORIZED = 401;
+const FORBIDDEN = 403;
 const NOT_FOUND = 404;
+const CONFLICT = 409;
 const INTERNAL_SERVER_ERROR = 500;
 
-module.export = {
+module.exports = {
+  CREATED,
   BAD_REQUEST,
+  UNAUTHORIZED,
+  FORBIDDEN,
   NOT_FOUND,
+  CONFLICT,
   INTERNAL_SERVER_ERROR,
 };
